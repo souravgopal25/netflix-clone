c24f09ec7031685f7185864000e74aa1a1338d9f
Sourav Sharma <souravgopal25@gmail.com>
2021-05-25 16:03:52 +0530

    Previews complete

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -52,14 +52,14 @@ class _HomeScreenState extends State<HomeScreen> {
         slivers: [
           SliverToBoxAdapter(
               child: ContentHeader(featuredContent: sintelContent)),
-          // SliverPadding(
-          //     padding: const EdgeInsets.only(top: 20.0),
-          //     sliver: SliverToBoxAdapter(
-          //       child: Previews(
-          //         title: 'Previews',
-          //         contentList: previews,
-          //       ),
-          //     ))
+          SliverPadding(
+              padding: const EdgeInsets.only(top: 20.0),
+              sliver: SliverToBoxAdapter(
+                child: Previews(
+                  title: 'Previews',
+                  contentList: previews,
+                ),
+              )),
         ],
       ),
     );
